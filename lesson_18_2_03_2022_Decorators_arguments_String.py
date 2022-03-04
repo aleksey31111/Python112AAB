a37aa431e08aaa9afda11df67443851e8f20fa2a
Aleksey <aleksejbaskirov31@gmail.com>
2022-03-05 02:59:55 +0300

    Start VIEW lesson_18_2_03_2022_Decorators_arguments_String.py Restrict DATA TYPE By Decorator

--- lesson_18_2_03_2022_Decorators_arguments_String.py
+++ lesson_18_2_03_2022_Decorators_arguments_String.py
@@ -1,60 +1,58 @@
 ##### LESSON 18
-## CONTINUE
+## CONTINUED Decorator Explanaition
+### Function that Is Decorator Adding Action to DECORATING FUNTCTION ###
 ##### DECORATOR With MORE Argument #####
 # def args_decorator(fn):
 #     def wrap(*args, **kwargs):
-#         print("args: ",args)
+#         print("args: ", args)
 #         print("kwargs: ", kwargs)
 #         fn(*args, **kwargs)
 #
 #     return wrap
 #
+#
 # @args_decorator
 # def print_full_name(a, b, c, study="Python"):
 #     print(a, b, c, "STADY", study, "\n")
-#
-#
-#
-#
 # print_full_name("Boris", "Elka", "Svetka", study="JavaScript")
 # print_full_name("Vlad", "Kate", "Victor")
 
+
+### DECORATOR Takes PARAMETERS ###
+# def args_decorator(arg1, arg2):  # Parameter Of Decorator
+#     print("Апгументы 'def args_decorator(arg1, arg2)': ", arg1, arg2)  # "Игорь Нефёдов"
 #
-# def args_decorator(arg1, arg2):  # Igor Nefedov
-#     print("Arguments: ", arg1, arg2)
+#     def wrapper(func1):  # Function
 #
-#     def wrapper(func1):
-#         def wrap(func_arg1, func_arg_2):
-#             print("Arguments of Function: ", func_arg1, func_arg_2)
+#         def wrap(func_arg1, func_arg_2):  # "Ирина", "Лаврова"
+#             print("Аргументы 'def wrap(func_arg1, func_arg_2: ", func_arg1, func_arg_2)
 #             func1(func_arg1, func_arg_2)
-#             # return func1(func_arg1, func_arg_2)
-#             return func1(arg1, arg2)
+#             func1(arg1, arg2)
+#
+#         #         return func1(arg1, arg2)
 #
 #         return wrap
 #
 #     return wrapper
 #
 #
-# @args_decorator("Igor", "Nefedov")
+# @args_decorator("Игорь", "Нефёдов")
 # def func(first, last):
-#     print("My name is: ", first, last)
+#     print("Меня зовут: ", first, last)
 #
 #
-# print("Irina", "Lavrova")
+# func("Ирина", "Лаврова")
 
-
-### Decorator 2
-#
+### Decorator 2 with Parameters By Default.
 # def args_decorator(decorator_text):
 #     def wrapper(func):
-#         def wrap(*args):
+#         def wrap(*args):  # (a)
 #             print(decorator_text, end="")
-#             func(*args)
+#             func(*args)  # (a)
+#             print()
+#
 #         return wrap
 #     return wrapper
-#
-#
-#
 #
 #
 # @args_decorator(decorator_text="Hello, ")
@@ -62,43 +60,82 @@
 #     print(text)
 #
 # @args_decorator(decorator_text="Hi, ")
-# def hello_world(t2):
+# def hello(text, t2):
 #     print(text, "and", t2)
-#
+# #
 # hello_world("world")
 # hello("Igor", "Irina")
 
 
 # ### Task 1 :
-# def multiply(a):
+print("Создать ДЕКОРАТОР, который Будет Принимать в Виде АРГУМЕНТА 'ЧИСЛО',"
+      "которое Будет Умножаться На ЧИСЛО Принимаемое ФУНКЦИЕЙ")
 
 
-# ##### Restrict ####
-# def typed(*args, **kwargs):
-#     print(*args)
-#     print(**kwargs)
-#     def wrapper(fn):
-#         def wrap(*f_args, **f_kwargs):
-#             for i in range(len(args)):
-#                 if type(f_args[i]) != args[i]:
-#                     print("Некорректный тип данных")
-#                     raise TypeError("Некорректный тип данных")
-#             for k in range(len(kwargs)):
-#                 raise TypeError("Некорректный тип данных")
-#             for k in kwargs:
-#                 if type(f_args[i]) != args[i]:
-#                     print("Некорректный тип данных")
-#                     raise TypeError("Некорректный тип данных")
-#             return fn(*f_args, **f_kwargs)
+# ## Variant 1: DECORATOR takes Parameter ##
+# def multiply(arg1):
+#     def wrapper(func):
+#         def wrap(func_arg1):
+#             return arg1 * func_arg1
 #
 #         return wrap
 #
 #     return wrapper
 #
 #
-# @typed(int, int, int)
-# def typed_fn(x, y, z):
-#     return x * y * z
+# @multiply(3)
+# def return_num(num):
+#     return num
+#
+#
+# print(return_num(5))
+#
+## Variant 2 # On Lesson 18 2.03.2022
+# def multiply(a):
+#     def wrapper(func):
+#         def wrap(b):
+#             return a * func(b)  # b
+#
+#         return wrap
+#
+#     return wrapper
+#
+#
+# @multiply(3)
+# def return_num(num):
+#     return num
+#
+#
+# print(return_num(5))
+
+
+# ##### Restrict ####  ONLY CERTAIN Data Type #####
+def typed(*args, **kwargs):
+#     print(*args)
+#     print(**kwargs)
+    def wrapper(fn):
+        def wrap(*f_args, **f_kwargs):
+            for i in range(len(args)):
+                if type(f_args[i]) != args[i]:
+                    # print("Некорректный тип данных")
+                    # pass
+                    raise TypeError("Некорректный тип данных")
+#             for k in range(len(kwargs)):
+#                 raise TypeError("Некорректный тип данных")
+#             for k in kwargs:
+#                 if type(f_args[i]) != args[i]:
+#                     print("Некорректный тип данных")
+#                     raise TypeError("Некорректный тип данных")
+            return fn(*f_args, **f_kwargs)
+
+        return wrap
+
+    return wrapper
+
+
+@typed(int, int, int)
+def typed_fn(x, y, z):
+    return x * y * z
 #
 #
 # @typed(str, str, str)
@@ -110,8 +147,8 @@
 #     return x + y + z
 #
 #
-# # print(typed_fn(3, 4, 5))
-# # print(typed_fn(3, 4, "Hello"))
+print(typed_fn(3, 4, 5))
+print(typed_fn(3, 4, "Hello"))
 # # print(typed_fn2("Hello, ", "world", "!"))
 # # print(typed_fn2("Hello, ", "world", 2))
 # print(typed_fn3("Hello, ", "world!  ", z=2))
@@ -196,23 +233,25 @@
 # s=s[:3]+'t'+s[4]
 # print(s)
 
-def change_char_to_str(s, old, new):
-    s2 = ""
-    i = 0
-    while i < len(s):
-        if s[i] == old:
-            s2 = s2 + new
-        else:
-            s2 = s2 + s[i]
-        i = i + 1
+# def change_char_to_str(s, old, new):
+#     s2 = ""
+#     i = 0
+#     while i < len(s):
+#         if s[i] == old:
+#             s2 = s2 + new
+#         else:
+#             s2 = s2 + s[i]
+#         i = i + 1
+#
+#     return s2
+#
+#
+# str1 = "Я изучаю Nuthon. Мне нравится Nuthon.  Nuthon очень интерестный"
+# str2 = change_char_to_str(str1, "N", "P")
+# print("str1 =", str1)
+# print("str2 =", str2)
 
-    return s2
 
-
-str1 = "Я изучаю Nuthon. Мне нравится Nuthon.  Nuthon очень интерестный"
-str2 = change_char_to_str(str1, "N", "P")
-print("str1 =", str1)
-print("str2 =", str2)
 # for i in str1:
 #     if i == "N":
 #         str2.append.str1[:i] + "P" + str1[i + 1::]
