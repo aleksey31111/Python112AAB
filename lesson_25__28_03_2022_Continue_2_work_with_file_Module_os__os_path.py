4ebd539b3bc1c34ce5582dc0758e00b67e121d97
Aleksey <aleksejbaskirov31@gmail.com>
2022-04-04 23:50:27 +0300

    ADD lesson_27__04.04.2022.py

--- lesson_25__28_03_2022_Continue_2_work_with_file_Module_os__os_path.py
+++ lesson_25__28_03_2022_Continue_2_work_with_file_Module_os__os_path.py
@@ -1,4 +1,4 @@
-### Lesson 25: 28 Marth 2022 yers. ###
+ ### Lesson 25: 28 Marth 2022 yers. ###
 ### Work with Several File with Replace:
 # text = "Строка №1\nСтрока №2\nСтрока №3\nСтрока №4\nСтрока №5\nСтрока №6\nСтрока №7\nСтрока №8\nСтрока №9\nСтрока №10\n"
 # with open('one.txt', 'w') as f:
@@ -16,7 +16,7 @@
 ### Work with 'Path' and 'Directory' ###
 ### Moduls: os, os.path ###
 
-# import os
+import os
 # print("Тукущая директория: ", os.getcwd())
 # print(os.listdir())
 # print(os.listdir(".."))
@@ -36,30 +36,30 @@
 #     print("Subdirs: ", dirs)
 #     print("Files: ", files)
 
-# for root, dirs, files in os.walk('Work2', topdown=False):
-#     print("Root: ", root)
-#     print("Subdirs: ", dirs)
-#     print("Files: ", files)
+for root, dirs, files in os.walk('Work2', topdown=False):
+    print("Root: ", root)
+    print("Subdirs: ", dirs)
+    print("Files: ", files)
 
 
-# for root, dirs, files in os.walk('Work'):
-#     if not os.listdir(root)
-#         os.rmdir(root)
-#         print(f"Директория {root} удалена.")
+for root, dirs, files in os.walk('Work'):
+    if not os.listdir(root):
+        os.rmdir(root)
+        print(f"Директория {root} удалена.")
 import os
 import os.path
 
 # /home/Work/test.txt - linux, Mac
 # D:\Python112\Work\F5
 #
-# print(os.path.split(r"i:\PythonCours\Python\pythonProject\Work2\F5\1.txt"))
+print(os.path.split(r"i:\PythonCours\Python\pythonProject\Work2\F5\1.txt"))
 # print(os.path.split(r"i:\PythonCours\Python\pythonProject\Work2\F5"))
 # print(os.path.split(r"i:\PythonCours\Python\pythonProject\Work2\F5")[0])
 # print(os.path.split(r"i:\PythonCours\Python\pythonProject\Work2\F5")[1])
 # print(os.path.split(r"i:\PythonCours\Python\pythonProject\Work2\F5\1.txt")[0])
 # print(os.path.split(r"i:\PythonCours\Python\pythonProject\Work2\F5\1.txt")[1])
 #
-# print(os.path.dirname(r"i:\PythonCours\Python\pythonProject\Work2\F5\1.txt"))
+print(os.path.dirname(r"i:\PythonCours\Python\pythonProject\Work2\F5\1.txt"))
 # print(os.path.basename(r"i:\PythonCours\Python\pythonProject\Work2\F5\1.txt"))
 #
 # print(os.path.join(r"i:\PythonCours\Python\pythonProject", "Work2", "F5", "1.txt"))
@@ -82,6 +82,7 @@ import os.path
 #         open(file_path, 'w').close()
 #         print(file_path)
 #         print()
+
 
 # print("### wALK OFF Dir")
 # file_with_text = ['Work/w.txt', 'Work/F1/f12.txt', 'Work/F2/F21/f211.txt', "Work/F2/F21/f212.txt"]
@@ -129,28 +130,28 @@ import os.path
 # print(os.path.getmtime('i:\PythonCours\Python\pythonProject\Work/F2//F21/f211.txt'))
 # # Time Created File
 # print(os.path.getctime('i:\PythonCours\Python\pythonProject\Work/F2//F21/f211.txt'))
-
-import time
-
-path = r"i:\Musik\Alisa\Sound\#10_Seychas pozdnee chem ti dumaesh (2003g.)\#10_02_Nebo slavyan.mp3"
-size = os.path.getsize(path)
-ksize = size // 1024
-print(f"Размер {ksize} Kb")
-
-c = os.path.getctime(path)
-b = time.strftime("%d.%m.%Y, %H:%M:%S", time.localtime(c))
-# time.strftime("%d.%m.%Y, %H:%M:%S", time.localtime())
-print(b)
-
-print("Task 4:")
-n_f = "i:\PythonCours\Python\pythonProject\lesson_24__23.03.2022_File_read_write_close.py"
-if os.path.exists(n_f):
-    d, name = os.path.split(n_f)
-    a = os.path.getctime(n_f)
-    print(f"{name} ({d}) - last access time {a} sec")
-
-else:
-    print(f"File {n_f} Not Found")
+#
+# import time
+#
+# path = r"i:\Musik\Alisa\Sound\#10_Seychas pozdnee chem ti dumaesh (2003g.)\#10_02_Nebo slavyan.mp3"
+# size = os.path.getsize(path)
+# ksize = size // 1024
+# print(f"Размер {ksize} Kb")
+#
+# c = os.path.getctime(path)
+# b = time.strftime("%d.%m.%Y, %H:%M:%S", time.localtime(c))
+# # time.strftime("%d.%m.%Y, %H:%M:%S", time.localtime())
+# print(b)
+#
+# print("Task 4:")
+# n_f = "i:\PythonCours\Python\pythonProject\lesson_24__23.03.2022_File_read_write_close.py"
+# if os.path.exists(n_f):
+#     d, name = os.path.split(n_f)
+#     a = os.path.getctime(n_f)
+#     print(f"{name} ({d}) - last access time {a} sec")
+#
+# else:
+#     print(f"File {n_f} Not Found")
 # print("Name File: ", n_f[-46:])
 # print("Dirs name: ", n_f[-20:-10])
 # print(os.path.getatime(n_f))
@@ -178,5 +179,5 @@ else:
 
 
 ### Continue Lesson ###
-print(os.path.isfile(r'i:\PythonCours\Python\pythonProject\Work/F2/F21/f211.txt'))
-print(os.path.isdir(r'i:\PythonCours\Python\pythonProject\Work/F2/F21'))
+# print(os.path.isfile(r'i:\PythonCours\Python\pythonProject\Work/F2/F21/f211.txt'))
+# print(os.path.isdir(r'i:\PythonCours\Python\pythonProject\Work/F2/F21'))
