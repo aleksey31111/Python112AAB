c298bee68cdd81492c5a6bf1a649941c406a335b
Aleksey <aleksejbaskirov31@gmail.com>
2022-04-27 22:36:26 +0300

    ADD lesson_34__27_04_2022_Module_Packege_Serializate_Deserializate_Pack_unpack_picle.py with Files

---
+++ car/carclass.py
@@ -0,0 +1,17 @@
+# from car import electrocar
+#
+# class CarClass:
+#     def __init__(self, brand, model, year, probeg):
+#         self.brand = brand
+#         self.model =model
+#         self.year = year
+#         self.probeg = probeg
+#
+#     def show_car(self):
+#         print(f"{self.brand}, {self.model}, {self.year}, {self.probeg}")
+#
+#
+# if __name__ == '__main__':
+#     e_car = electrocar.ElectroCar("Tesla", "T", 2022, 1199000)
+#     e_car.show_car()
+#     e_car.description_battery()
