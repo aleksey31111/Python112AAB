c5d2cb2642ab049ebb787ae8f5a9a970bebf0c26
Aleksey <aleksejbaskirov31@gmail.com>
2022-06-25 01:14:51 +0300

    VIEW  flaskapp_lesson_46/: main.py, static/css/style.css, templates/about.html, templates/base.html, templates/index.html

--- flaskapp_lesson_46/main.py
+++ flaskapp_lesson_46/main.py
@@ -1,27 +1,33 @@
 from flask import Flask, render_template, url_for
 
-
 app = Flask(__name__)
 
+# menu = ["Главная", "О нас", "Обратная связь"]
 menu = [
-           {"name": "лавная", "url": "index"},
-           {"name":"О нас", 'url':'about'},
-           {"name":"Обратная связь", 'url':'contact'}]
-
+    {"name": "Главная", "url": "index"},
+    {"name": "О нас", 'url': 'about'},
+    {"name": "Обратная связь", 'url': 'contact'}]
 
 
 @app.route("/index")
 @app.route("/")
 def index():
+    print(url_for('index'))
     return render_template("index.html", title="Главная", menu=menu)
+    # return "index"
+
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title="Описание", menu=menu)
+    print(url_for('about'))
+    return render_template("about.html", title="О сайте", menu=menu)
+    # return "<h1>О сайте</h1>"
 
-@app_route("/profile/<int:username>")
+
+@app.route("/profile/<username>")
 def profile(username):
-    return f"Username: {username}"
+    # print(url_for("profile/<username>"))
+    return f"Пользователь: {username}"
 
 
 if __name__ == '__main__':
