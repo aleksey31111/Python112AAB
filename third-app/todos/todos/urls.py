c22a906c27463f3f93e94e0ed42a73e3748680e6
Aleksey <aleksejbaskirov31@gmail.com>
2022-07-06 23:27:42 +0300

    ADD LESSON 50

--- third-app/todos/todos/urls.py
+++ third-app/todos/todos/urls.py
@@ -1,4 +1,4 @@
-"""personal_portfolio URL Configuration
+"""todos URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
-from skills import views
+from django.urls import path
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('blog/', include('blog.urls')),
-]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Auth
+    path('signup', views.signupuser, name='signupuser'),
+
+    # Todos
+    path('current/', views.currenttodos, name='currenttodos'),
+]
