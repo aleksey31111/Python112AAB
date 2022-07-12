182017eb185a0f637172a28d17690bf24e3a6215
Aleksey <aleksejbaskirov31@gmail.com>
2022-07-13 00:29:49 +0300

    VIEW Lesson_52 and Lesson_51 and Lesson_50

--- second-app/personal_portfolio/blog/views.py
+++ second-app/personal_portfolio/blog/views.py
@@ -9,4 +9,4 @@ def blogs(request):
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'blog/details.html', {'blog': blog_id})
+    return render(request, 'blog/details.html', {'blog': blog})
